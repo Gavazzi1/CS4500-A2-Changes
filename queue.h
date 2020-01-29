0883cc63697b7646cfa3162f6c2931424a707daa
Anthony Gavazzi <gavazzi.a@husky.neu.edu>
2020-01-28 19:36:05 -0500

    modified queue and queue-test according to github requests

---
+++ queue.h
@@ -0,0 +1,106 @@
+//lang::CwC
+
+#pragma once
+
+#include "object.h"
+#include "queuetype.h"
+
+/**
+ * Represents a queue data structure, in which elements are ordered in a
+ * first-in, first-out (FIFO) manner.
+ */
+class Queue : public Object {
+  public:
+    /**
+     * Constructs a new Queue object.
+     *
+     * @param type the type of element that this Queue will contain
+     */
+    Queue(QueueType type) {}
+
+    /**
+     * Deletes this Queue object, freeing up the memory it took up.
+     */
+    virtual ~Queue() {}
+
+    /**
+     * Returns the type of element that will  be stored in this Queue.
+     *
+     * @return a QueueType indicating what type the Queue will contain
+     */
+    QueueType getType() {}
+
+    /**
+     * Adds the given Object into this Queue, returning true if the insertion
+     * was successful. We return a bool becuase although this interface does not
+     * specify a maximum capacity, derived classes may wish to do so. This Queue
+     * takes ownership of the given pointer and must free it in the
+     * deconstructor.
+     *
+     * @param o the Object* to added to the Queue
+     */
+    bool add(Object* o) {}
+
+    /**
+     * Returns the Object at the head of the Queue, but does not remove the object 
+     * or modify the head. Returns nullptr if the Queue is empty.
+     * Ownership of the returned value stays with this Queue.
+     *
+     * @return a pointer to the element at the head of the Queue
+     */
+    Object* peek() {}
+
+    /**
+     * Removes and returns the Object at the head of the Queue, modifying the
+     * head to point to the next Object, if there is one. Returns nullptr if the
+     * Queue is empty. Ownership of the popped value goes to the function that
+     * called pop();
+     *
+     * @return a pointer to the element at the head of the Queue
+     */
+    Object* pop() {}
+
+    /**
+     * Returns whether this Queue is empty (has no elements)
+     *
+     * @return whether this Queue is empty
+     */
+    bool isEmpty() {}
+
+    /**
+     * Returns the size of this Queue (the number of elements)
+     *
+     * @return the number of elements in this Queue
+     */
+    size_t size() {}
+
+    /**
+     * Returns whether this Queue is equal to the given Object. This means that
+     * o points to another Queue, both Queues have the same number of elements,
+     * and the elements (in order) are equal.
+     *
+     * @param o the Object to compare to this Queue
+     * @return whether this Queue is equal to the given Object
+     */
+    bool equals(Object* o) {}
+
+    /**
+     * Returns a unique hash code for this Queue
+     *
+     * @return a hash code for this Queue
+     */
+    int hashCode() {}
+
+    /**
+     * Returns a string literal representing this Queue.
+     * The string should be surrounded by [], and consist of the output of each
+     * element's toString method separated by commas and one space like so:
+     *
+     * [Element1, Element2, Element3]
+     *
+     * The elements are printed starting with the head of the Queue.
+     *
+     * @return a string literal representing this Queue
+     */
+    const char* toString() {}
+};
---
+++ test-queue.cpp
@@ -0,0 +1,162 @@
+//lang::CwC
+
+#include <iostream>
+#include <exception>
+#include "queue.h"
+#include "queuetype.h"
+
+void FAIL() {   exit(1);    }
+void OK(const char* m) { 
+	std::cout << "OK: " << m << std::endl;
+}
+void t_true(bool p) { if (!p) FAIL(); }
+void t_false(bool p) { if (p) FAIL(); }
+
+void testGetType() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	t_true(q1->getType() == QueueType::OBJECT);
+	Queue* q2 = new Queue(QueueType::STRING);
+	t_true(q2->getType() == QueueType::STRING);
+	OK("getType()");
+	delete q1;
+	delete q2;
+}
+
+void testAdd() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	Object* obj = new Object();
+	bool added = q1->add(obj);
+	t_true(added);
+	OK("add()"); 
+	delete q1;
+}
+
+void testPeekOnEmpty() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	t_true(q1->size() == 0);
+    t_true(q1->peek() == nullptr);
+    OK("peek() on empty Queue");
+	delete q1;
+}
+
+void testPeek() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	Object* obj1 = new Object();
+
+	q1->add(obj1);
+	t_true(q1->size() == 1);
+
+	Object* obj = q1->peek();
+
+	t_true(obj->equals(obj1));
+	t_true(q1->size() == 1);
+
+	OK("peek()");
+    delete q1;
+}
+
+void testPopOnEmpty() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	t_true(q1->size() == 0);
+    t_true(q1->pop() == nullptr);
+    OK("pop() on empty Queue");
+	delete q1;
+}
+
+void testPop() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	Object* obj1 = new Object();
+
+	q1->add(obj1);
+	t_true(q1->size() == 1);
+
+	Object* obj = q1->pop();
+
+	t_true(obj->equals(obj1));
+	t_true(q1->size() == 0);
+
+	OK("pop()");
+    delete obj;
+	delete q1;
+}
+
+void testIsEmpty() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	Object* obj1 = new Object();
+	t_true(q1->isEmpty());
+	q1->add(obj1);
+	t_false(q1->isEmpty());
+	OK("isEmpty()");
+	delete q1;
+}
+
+void testSize() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	Object* obj1 = new Object();
+	Object* obj2 = new Object();
+	t_true(q1->size() == 0);
+	q1->add(obj1);
+	t_true(q1->size() == 1);
+	q1->add(obj2);
+	t_true(q1->size() == 2);
+	q1->pop();
+	t_true(q1->size() == 1);
+	q1->pop();
+	t_true(q1->size() == 0);
+
+	OK("size()");
+	delete q1;
+}
+
+void testEquals() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+	Queue* q2 = new Queue(QueueType::OBJECT);
+	Queue* q3 = q1;
+
+	t_true(q1->equals(q2)); // empty queues are equal
+	t_true(q1->equals(q3)); // same pointer
+
+	Object* obj1 = new Object();
+	q1->add(obj1);
+	t_false(q1->equals(q2));
+	OK("equals()");
+	delete q2;
+	delete q1;
+}
+
+void testToString() {
+	Queue* q1 = new Queue(QueueType::OBJECT);
+    std::string s = q1->toString();
+	t_true(s == "[]");
+
+	Object* obj1 = new Object();
+	q1->add(obj1);
+    s = q1->toString();
+    t_true(s.at(0) == '[');
+    t_true(s.at(s.length() - 1) == ']');
+
+	Object* obj2 = new Object();
+	q1->add(obj2);
+    s = q1->toString();
+    t_true(s.at(0) == '[');
+    t_true(s.at(s.length() - 1) == ']');
+    t_true(s.find(", ") != std::string::npos); 
+
+	OK("toString()");
+	delete q1;
+}
+
+int main() {
+	testGetType();
+	testAdd();
+	testPeekOnEmpty();
+	testPeek();
+	testPopOnEmpty();
+	testPop();
+	testIsEmpty();
+	testSize();
+	testEquals();
+	testToString();
+
+	return 0;
+}
